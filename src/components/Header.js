9ef6dd9944cc8d5a5a5e5793b4b7c923342f1dbe
YasirSherzad <ys.sherzad@gmail.com>
2021-10-19 08:49:10 +0430

    add profile img in home header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import {
+    View,
+    StyleSheet,
+    TouchableOpacity,
+    Animated,
+    Image
+} from 'react-native';
 import MenuBar from '../../assets/menu-bar.svg';
 import BackIcon from '../../assets/arrow-left.svg';
 import { theme } from '../theme';
+import { elevation_3 } from '../utils';
 
 const HeaderIcon = {
     'home': <MenuBar height={25} width={25} fill={theme.icon_main} />,
@@ -11,6 +18,8 @@ const HeaderIcon = {
 
 const Header = (props) => {
     const { route, navigation, back, progress } = props;
+
+    const isHomePage = route.name === 'home';
 
     const pressed = () => {
         if (back) navigation.pop();
@@ -28,6 +37,11 @@ const Header = (props) => {
             <TouchableOpacity onPress={pressed}>
                 {Icon}
             </TouchableOpacity>
+
+            {isHomePage && (
+
+                <Image source={require('../../assets/image2.jpeg')} style={styles.profileImg} />
+            )}
         </Animated.View>
     );
 };
@@ -36,10 +50,22 @@ const styles = StyleSheet.create({
     container: {
         height: 50,
         paddingHorizontal: 28,
-        justifyContent: 'center',
-        alignItems: 'flex-start',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         backgroundColor: '#fff'
     },
+    flexOne: {
+        flex: 1
+    },
+    profileImg: {
+        height: 36,
+        width: 36,
+        borderRadius: 99,
+        resizeMode: 'cover',
+        marginRight: 2,
+        ...elevation_3
+    }
 });
 
 export default Header;
